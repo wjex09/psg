ebf0e2f3831f0f69b179a06cf0a90fa669664aba
Navodit <navoditn15@gmail.com>
2023-01-29 10:14:32 +0530

    init

---
+++ sol.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+  "fmt"
+)
+
+func solve()
+{
+  var a,b int
+  fmt.Scanln(&a,&b)
+  fmt.Printf("%d %d \n",a,b)
+}
+
+
+func main()
+{
+  t:=1
+  fmt.Fscan(getReader(),&t)
+  fmt.Fprintln()
+}
+
