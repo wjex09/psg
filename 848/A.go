2ad9b91a3bb7acc32bacefb53297a6a30a04cd38
Navodit <navoditn15@gmail.com>
2023-02-03 23:24:08 +0530

    finally solve A

--- 848/A.go
+++ 848/A.go
@@ -1,24 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"math"
+	"os"
+)
+
+var (
+	in  = bufio.NewReader(os.Stdin)
+	out = bufio.NewWriter(os.Stdout)
 )
 
 func solve() {
 	var n int
-	fmt.Scanf("%d", &n)
-	var q []int
+	fmt.Fscan(in, &n)
+	acc := 0
+	var arr = make([]int, n)
 	for i := 0; i < n; i++ {
-		fmt.Scanf("%d", &q[i])
+		fmt.Fscan(in, &arr[i])
+		acc += arr[i]
 	}
-	for i, v := range q {
-		fmt.Println("%d %d", i, v)
+
+	ans := -1e9 + 7
+	for i := 0; i < n-1; i++ {
+		ans = math.Max(ans, float64(acc-2*(arr[i]+arr[i+1])))
 	}
+	fmt.Fprintln(out, ans)
 }
 
 func main() {
+	defer out.Flush()
 	t := 1
-	fmt.Scanf("%d", &t)
+	fmt.Fscan(in, &t)
 	for i := 0; i < t; i++ {
 		solve()
 	}
