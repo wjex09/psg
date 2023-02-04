6a565cfe7269e7de0f551fc0a40b32befab45725
Navodit <navoditn15@gmail.com>
2023-02-04 15:08:30 +0530

    min max func

--- template.go
+++ template.go
@@ -12,6 +12,17 @@ var (
 )
 
 func solve() {
+	var n, m, d int
+	fmt.Fscan(in, &n, &m, &d)
+	p := make([]int, n)
+	a := make([]int, m)
+	for i := 0; i < n; i++ {
+		fmt.Fscan(in, &p[i])
+	}
+	for i := 0; i < m; i++ {
+		fmt.Fscan(in, &a[i])
+	}
+	//fmt.Println(p, a)
 }
 
 func main() {
@@ -21,4 +32,45 @@ func main() {
 	for i := 0; i < t; i++ {
 		solve()
 	}
+}
+
+type Signed interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64
+}
+
+type Unsigned interface {
+	~uint | ~uint8 | ~uint16 | ~uint32 | ~uint64
+}
+
+type Integer interface {
+	Signed | Unsigned
+}
+
+type Float interface {
+	~float32 | ~float64
+}
+
+type Number interface {
+	Integer | Float
+}
+
+type Ordered interface {
+	Integer | ~string | Float
+}
+
+
+func min[T Ordered](x T, value ...T) T{
+	ret:=x
+	for _,val:= range value{
+		if ret > val { ret = val }
+	}
+	return ret
+}
+
+func max[T Ordered](x T, value ...T) T{
+	ret:=x
+	for _,val:= range value{
+		if ret < val { ret = val }
+	}
+	return ret
 }
