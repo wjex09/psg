bef7b158e8fc64961c565971348209e94ab056d3
Navodit <navoditn15@gmail.com>
2023-02-22 20:25:34 +0530

    template.go

--- template.go
+++ template.go
@@ -12,17 +12,7 @@ var (
 )
 
 func solve() {
-	var n, m, d int
-	fmt.Fscan(in, &n, &m, &d)
-	p := make([]int, n)
-	a := make([]int, m)
-	for i := 0; i < n; i++ {
-		fmt.Fscan(in, &p[i])
-	}
-	for i := 0; i < m; i++ {
-		fmt.Fscan(in, &a[i])
-	}
-	//fmt.Println(p, a)
+
 }
 
 func main() {
